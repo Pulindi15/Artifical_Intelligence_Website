e6f6157baacdfe28853a7fa538c5e53006d303cb
Pulindi Dhanesha <pulindidhanesha15@gmail.com>
2024-07-13 13:00:46 +0530

    Updates

---
+++ precache-manifest.0705101bb8ea713c8123a6a73099758b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbebb578c2f1c81275e1f4ea21728bc",
+    "url": "/Artifical_Intelligence_Website/index.html"
+  },
+  {
+    "revision": "955cbc84e54f3437003e",
+    "url": "/Artifical_Intelligence_Website/static/css/main.091a5ce1.chunk.css"
+  },
+  {
+    "revision": "96d7b40297fc3267cb1f",
+    "url": "/Artifical_Intelligence_Website/static/js/2.dfad08ef.chunk.js"
+  },
+  {
+    "revision": "955cbc84e54f3437003e",
+    "url": "/Artifical_Intelligence_Website/static/js/main.860dd9a8.chunk.js"
+  },
+  {
+    "revision": "2419615b1e65a00da700",
+    "url": "/Artifical_Intelligence_Website/static/js/runtime~main.e76d84ae.js"
+  },
+  {
+    "revision": "0848c116059dde0b2adebb8d54512224",
+    "url": "/Artifical_Intelligence_Website/static/media/ai.0848c116.png"
+  },
+  {
+    "revision": "d50d2c04172babe220a581e11edea621",
+    "url": "/Artifical_Intelligence_Website/static/media/blog01.d50d2c04.png"
+  },
+  {
+    "revision": "c98b11ff12389de71fd0c8e88a1ab5c9",
+    "url": "/Artifical_Intelligence_Website/static/media/blog02.c98b11ff.png"
+  },
+  {
+    "revision": "36e94a87c8264d21716cad53c582b1d2",
+    "url": "/Artifical_Intelligence_Website/static/media/blog03.36e94a87.png"
+  },
+  {
+    "revision": "9200ceabba955208cbebd9fbddded25b",
+    "url": "/Artifical_Intelligence_Website/static/media/blog04.9200ceab.png"
+  },
+  {
+    "revision": "b7334ef99e6fd77765292b5dd367ed04",
+    "url": "/Artifical_Intelligence_Website/static/media/blog05.b7334ef9.png"
+  },
+  {
+    "revision": "a74338805645d518bbeb874a434d6c8e",
+    "url": "/Artifical_Intelligence_Website/static/media/logo.a7433880.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/Artifical_Intelligence_Website/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "433cc6c39ada737d4ac3f1f613fa3e19",
+    "url": "/Artifical_Intelligence_Website/static/media/possibility.433cc6c3.png"
+  }
+]);
